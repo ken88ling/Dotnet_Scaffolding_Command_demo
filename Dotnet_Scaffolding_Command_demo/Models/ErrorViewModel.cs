020b4242a7f7cd43dfd89a1f2cd037b164bb1d1b
Ken Ling <ken88ling@gmail.com>
2019-03-08 16:28:06 +0800

    Add project files.

---
+++ Dotnet_Scaffolding_Command_demo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dotnet_Scaffolding_Command_demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
