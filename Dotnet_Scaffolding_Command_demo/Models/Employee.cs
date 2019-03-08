31a89eee876f905de6b2e762bef42cd8f9baa48f
Ken Ling <ken88ling@gmail.com>
2019-03-08 16:31:22 +0800

    add model and dependency

---
+++ Dotnet_Scaffolding_Command_demo/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet_Scaffolding_Command_demo.Models
+{
+    public class Employee
+    {
+        public string Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Salary { get; set; }
+    }
+}
